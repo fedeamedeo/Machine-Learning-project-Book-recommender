d44813d5de74c45382fb9089ad3ffd293304c5a2
fedeamedeo <federica.amedeopriv@gmail.com>
2025-05-15 17:13:14 +0200

    e

--- app3.py
+++ app3.py
@@ -29,11 +29,12 @@ if "favorites" not in st.session_state:
 @st.cache_data
 def load_data():
     recs = pd.read_csv("tf_idf.csv")
-    items = pd.read_csv("books_complete.csv")
+    items = pd.read_csv("final_items.csv")
     interactions = pd.read_csv("interactions_train1.csv")
     return recs, items, interactions
 
 recs_df, items_df, interactions_df = load_data()
+
 
 # ---------- SIDEBAR ----------
 st.sidebar.title("Book Recommendations")
