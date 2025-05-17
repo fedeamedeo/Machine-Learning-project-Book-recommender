f45190f1c3d1456b003211459b400df844702765
fedeamedeo <federica.amedeopriv@gmail.com>
2025-05-17 18:32:38 +0200

    w

--- appbook_complete.py
+++ appbook_complete.py
@@ -88,6 +88,7 @@ st.sidebar.markdown("Welcome to the Book Recommender! Explore personalized book 
 st.sidebar.markdown("Select your Personal Library User ID to see book recommendations just for you.")
 user_id = st.sidebar.selectbox("User ID", recs_df['user_id'].unique())
 
+
 # ---------- RENDER BOOK SCROLLABLE ----------
 def render_books_vertical(df, prefix, allow_expansion=True):
     rows = [df.iloc[i:i+3] for i in range(0, len(df), 3)]
